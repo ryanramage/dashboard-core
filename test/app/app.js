b6177d9590f57651d62e53677c8da37094783225
Ryan Ramage <ryan.ramage@gmail.com>
2012-03-15 22:36:05 -0600

    Testing the _info url

---
+++ test/app/app.js
@@ -0,0 +1,23 @@
+var dashboard_views = require('lib/dashboard_views');
+var dashboard_shows = require('lib/dashboard_shows');
+var dashboard_updates = require('lib/dashboard_updates');
+var dashboard_rewrites = require('lib/dashboard_rewrites');
+
+exports.views = {
+    by_active_install : dashboard_views.by_active_install,
+    app_version_by_market : dashboard_views.app_version_by_market,
+    by_markets : dashboard_views.by_markets,
+    by_roles : dashboard_views.by_roles
+};
+
+exports.shows = {
+    redirectRoot : dashboard_shows.redirectRoot,
+    configInfo : dashboard_shows.configInfo,
+    info : dashboard_shows.info
+}
+
+exports.updates = {
+    modifyAppRewrites : dashboard_updates.modifyAppRewrites
+}
+
+exports.rewrites = dashboard_rewrites.getNeededRewrties('dashboard-core-test');
--- test/spec/app_test.js
+++ test/spec/app_test.js
@@ -1,10 +1,22 @@
 
 
-describe("All the stuff you want", function() {
+describe("Rewrites", function() {
 
-  it("should switch highlighted tabs", function() {
-
-        expect(true).toBeTruthy();
+  it("/_info should return a jsonp with dashboard info and dashboard=true", function() {
+      runs(function(){
+          $.ajax({
+              url :  '../_rewrite/_info',
+              dataType : 'json',
+              jsonp : true,
+              success : function(remote_data) {
+                   expect(remote_data.dashboard).toBeTruthy();
+                  expect(remote_data.config).toBeDefined();
+              },
+              error : function() {
+                    expect(true).toBeFalsy();
+              }
+          });
+      });
   });
 
 });
