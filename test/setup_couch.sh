c7136569e7a49f2e6807dd490413b211eb0b60e3
Ryan Ramage <ryan.ramage@gmail.com>
2012-03-16 19:39:04 -0600

    Fixing quotes

--- test/setup_couch.sh
+++ test/setup_couch.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -X PUT http://localhost:5984/_config/httpd/secure_rewrites -H 'Content-Type: application/json' -d 'false'
+curl -X PUT http://localhost:5984/_config/httpd/secure_rewrites -H 'Content-Type: application/json' -d '"false"'
