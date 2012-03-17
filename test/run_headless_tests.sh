8fb70da61160104f1d15470457f1c383c3441827
Ryan Ramage <ryan.ramage@gmail.com>
2012-03-16 18:28:37 -0600

    more logging cause travis is buggin me!

--- test/run_headless_tests.sh
+++ test/run_headless_tests.sh
@@ -2,4 +2,5 @@
 cd test
 kanso push "$1"
 phantomjs --version
+curl "$1/_design/dashboard-core-test/_rewrite/SpecRunner.html"
 phantomjs run-nodeunit-browser.js "$1/_design/dashboard-core-test/_rewrite/SpecRunner.html"
